91d96d910a9f464dc388eb3dc95bb34f8247295f
Mykhailo Dudash <dudasmika17@gmail.com>
2025-07-01 12:56:42 +0200

    Models and Basic CRUD operations

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\Api\FilmController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('films', [FilmController::class, 'index']);
+Route::get('films/{id}', [FilmController::class, 'show']);
+Route::post('films', [FilmController::class, 'store']);
+Route::put('films/{id}', [FilmController::class, 'update']);
+Route::delete('films/{id}', [FilmController::class, 'destroy']);
+Route::post('films/{id}/publish', [FilmController::class, 'publish']);
