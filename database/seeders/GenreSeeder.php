f52a3a604bf02479d30b8b8503032eac3f457e55
Mykhailo Dudash <dudasmika17@gmail.com>
2025-07-01 11:16:35 +0200

    Seeders done

---
+++ database/seeders/GenreSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class GenreSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $genres = ['Action', 'Comedy', 'Drama', 'Horror', 'Sci-Fi'];
+        foreach ($genres as $genre) {
+            DB::table('genres')->insert([
+                'name' => $genre,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
